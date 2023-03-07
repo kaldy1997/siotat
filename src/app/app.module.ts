8f03c2953b705bf19fbd36cbaddcd80a8dba9508
Claudio Higón <claudio.higon@ezzing.com>
2023-03-07 19:50:12 +0100

    chore(siotat): carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AudiovisualWorksComponent } from './modules/audiovisual-works/audiovisual-works.component';
 import { HomeComponent } from './modules/home/home.component';
 import { LabComponent } from './modules/lab/lab.component';
+import { CarouselComponent } from './shared/carousel/carousel.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { CarouselComponent } from './shared/carousel/carousel.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,9 @@ import { CarouselComponent } from './shared/carousel/carousel.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/carousel/carousel.component.ts
+++ src/app/shared/carousel/carousel.component.ts
@@ -1,16 +1,52 @@
 import { Component } from '@angular/core';
-import { carouselImage } from 'src/app/models/carousel-image.model';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
-  selector: 'app-carousel',
-  templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.sass']
+    selector: 'app-carousel',
+    templateUrl: './carousel.component.html',
+    styleUrls: ['./carousel.component.sass']
 })
 export class CarouselComponent {
 
-  public logo: string;
-  constructor() {
-    this.logo = 'LOGO'
-  }
+    public logo: string;
+    constructor() {
+        this.logo = 'LOGO'
+    }
 
+    public images: string[] = [
+        'assets/img/_MG_2241.png',
+        'assets/img/_MG_9056.JPG',
+        'assets/img/DSC00291.JPG',
+        'assets/img/DSC00363.JPG',
+        'assets/img/DSC01406.png',
+        'assets/img/DSC03531.jpg',
+        'assets/img/DSC03638.jpg',
+        'assets/img/DSC03653-2.jpg',
+    ];
+
+    public customOptions: OwlOptions = {
+        loop: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        autoplayTimeout: 2000,
+        fluidSpeed: true,
+        autoWidth: true,
+        autoHeight: true,
+        dots: false,
+        mouseDrag: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            100: {
+                items: 2
+            },
+            200: {
+                items: 3
+            },
+            300: {
+                items: 4
+            }
+        }
+    };
 }
